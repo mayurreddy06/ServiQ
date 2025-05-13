226caefdb0c38db622e655d0a189d8904bc302a9
Mayur R <msreddy3067@gmail.com>
2025-05-13 15:21:48 -0400

    Modified gitignore

---
+++ client/scripts/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDOG7sCxQqRTTWEK2AL3Q7MHa5Pg_ZflxQ',
+  authDomain: 'store-discount-finder.firebaseapp.com',
+  databaseURL: 'https://store-discount-finder-default-rtdb.firebaseio.com/',
+  projectId: 'store-discount-finder',
+  storageBucket: 'store-discount-finder.firebasestorage.app',
+  messagingSenderId: '615316707665',
+  appId: '1:615316707665:web:0ae7d08c651e12bad31b70',
+  measurementId: 'G-6F8RV8NVNP'
+};
